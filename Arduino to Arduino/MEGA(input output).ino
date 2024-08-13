88ddfc8763c1bca6ef59acfeca0c4922e7cd50da
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-08-12 22:57:19 -0500

    Update MEGA(input output).ino

--- Arduino to Arduino/MEGA(input output).ino
+++ Arduino to Arduino/MEGA(input output).ino
@@ -1,3 +1,4 @@
+
 //RELAYS ON BOARD A and C
 #define RELAY_A_R1 46 //Let's pretend it is Board A and name for relay = RELAY_A_R1 where A-name of the board and R1-Red LED number 1
 #define RELAY_A_G1 48 //Let's pretend it is Board A and name for relay = RELAY_A_G1 where A-name of the board and G1-Green LED number 1
@@ -10,8 +11,8 @@
 
 
 //RELAYS ON BOARD D
-#define RELAY_D_G1 45
-#define RELAY_D_R1 43
+#define RELAY_D_R1 45
+#define RELAY_D_G1 43
 #define RELAY_D_G2 41
 #define RELAY_D_R2 39
 
@@ -27,11 +28,11 @@
 #define RFID1_A_G 8
 #define RFID1_A_R 9
 #define RFID2_A_G 10
-#define RFID2_A_G 11
+#define RFID2_A_R 11
 #define RFID1_C_G 12
-#define RFID1_C_G 13
+#define RFID1_C_R 13
 #define RFID2_C_G 22
-#define RFID2_C_G 24
+#define RFID2_C_R 24
 
 
 
@@ -74,9 +75,42 @@ void setup()
   pinMode(RELAY_C_G2, OUTPUT);
   pinMode(RELAY_C_R2, OUTPUT);
 
+ //--------TEST ALL RELAYS/LEDs ON-OFF--------
+  digitalWrite(RELAY_A_R1, HIGH);
+  delay(200);
+  digitalWrite(RELAY_A_G1, HIGH);
+  delay(600);
+  digitalWrite(RELAY_A_R1, LOW);
+  delay(200);
+  digitalWrite(RELAY_A_G1, LOW);
+  delay(200);
+  digitalWrite(RELAY_A_R2, HIGH);
+  delay(200);
+  digitalWrite(RELAY_A_G2, HIGH);
+  delay(200);
+  digitalWrite(RELAY_A_R2, LOW);
+  delay(200);
+  digitalWrite(RELAY_A_G2, LOW);
+  delay(500);
+
+
  
-  // digitalWrite(2, LOW);
-  // digitalWrite(3, LOW);
+  digitalWrite(RELAY_D_G2, HIGH);
+  delay(200);
+  digitalWrite(RELAY_D_R2, HIGH);
+  delay(200);
+  digitalWrite(RELAY_C_G1, HIGH);
+  delay(200);
+  digitalWrite(RELAY_C_R1, HIGH);
+  delay(200);
+  digitalWrite(RELAY_C_G2, HIGH);
+  delay(200);
+  digitalWrite(RELAY_C_R2, HIGH);
+  delay(200);
+  digitalWrite(RELAY_D_G1, HIGH);
+  delay(200);
+  digitalWrite(RELAY_D_R1, HIGH);
+
   // digitalWrite(4, LOW);
   // digitalWrite(5, LOW);
   // digitalWrite(6, HIGH);
@@ -87,51 +121,128 @@ void setup()
 }
 
 void loop() {
-  //print out the value of the pushbutton
 
-  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
-  // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
-  // button's pressed, and off when it's not:
+int RFID1_A_R_status = digitalRead(RFID1_A_R);
+int RFID1_A_G_status = digitalRead(RFID1_A_G);
+int RFID2_A_R_status = digitalRead(RFID2_A_G);
+int RFID2_A_G_status = digitalRead(RFID2_A_R);
+int RFID1_C_G_status = digitalRead(RFID1_C_G);
+int RFID1_C_R_status = digitalRead(RFID1_C_R);
+int RFID2_C_G_status = digitalRead(RFID2_C_G);
+int RFID2_C_R_status = digitalRead(RFID2_C_R);
 
-  //GREEN
-  if (RFID1_D_G == 1) 
+int RFID1_D_G_status = digitalRead(RFID1_D_G);
+int RFID1_D_R_status = digitalRead(RFID1_D_R);
+int RFID2_D_G_status = digitalRead(RFID2_D_G);
+int RFID2_D_R_status = digitalRead(RFID2_D_R);
+int RFID3_D_G_status = digitalRead(RFID3_D_G);
+int RFID3_D_R_status = digitalRead(RFID3_D_R);
+
+
+  //-------------------------------RED ON RFID 1 BOARD A and C
+  if (RFID1_A_G_status == HIGH)
   {
-    Serial.print("RFID_A_1 pin 53: ");
-    Serial.println(RFID_A_1);
-    digitalWrite(53, HIGH);
+    Serial.print("RFID1_R pin 8: ");
+    Serial.println(RFID1_A_R);
+    digitalWrite(RELAY_A_R1, HIGH);
   }
-  else if (RFID_A_1 == 0) 
+   else if (RFID1_A_G_status == LOW)
   {
-    Serial.print("RFID_A_1 pin 53: ");
-    Serial.println(RFID_A_1);
-    digitalWrite(53, LOW); 
+        digitalWrite(RELAY_A_R1, LOW); 
   } 
-    
-    
-  
-  //RED
-  if (RFID_A_2 == 1) {
-    digitalWrite(51, HIGH);
-    } 
-    else if (RFID_A_2 == 0) { 
-    digitalWrite(51, LOW); 
-  }  
-  //GREEN
-  if (RFID_A_3 == 1) {
-    digitalWrite(49, HIGH);           
-    } 
-    else if (RFID_A_3 == 0){
-    digitalWrite(49, LOW);           
-    
+  //------------------------------GREEN on RFID 1 BOARD A and C
+  if (RFID1_A_R_status == HIGH)
+  {
+    Serial.print("RFID_A_1 pin 9: ");
+    Serial.println(RFID1_A_R);
+    digitalWrite(RELAY_A_G1, HIGH);
   }
-  //RED
-  if (RFID_A_4 == 1) {
-    digitalWrite(47, HIGH);
-    } 
-    else if (RFID_A_4 == 0){
-    digitalWrite(47, LOW); 
-    
-  }
+  else if (RFID1_A_R_status == LOW)
+  {
+    digitalWrite(RELAY_A_G1, LOW); 
+  } 
 
+
+  //-------------------------------RED ON RFID 2 BOARD A and C
+  if (RFID2_A_R_status == HIGH)
+  {
+    Serial.print("RFID2_R pin 10: ");
+    Serial.println(RFID2_A_R);
+    digitalWrite(RELAY_A_R2, HIGH);
+  }
+   else if (RFID2_A_R_status == LOW)
+  {
+    digitalWrite(RELAY_A_R2, LOW); 
+  } 
+   //------------------------------GREEN on RFID 3 (RFID1_C_G) BOARD A and C
+  if (RFID1_C_G_status == HIGH)
+  {
+    Serial.print("RFID1_C_G_status");
+    Serial.println(RFID1_C_G);
+    digitalWrite(RELAY_C_G1, LOW);
+  }
+  else if (RFID1_C_G_status == LOW)
+  {
+    digitalWrite(RELAY_C_G1, HIGH); 
+  } 
+
+//-------------------------------RED ON RFID 3 (RFID1_C_R) BOARD A and C
+  if (RFID1_A_R_status == HIGH)
+  {
+    Serial.print("RFID1_A_R_status");
+    Serial.println(RFID1_A_R_status);
+    digitalWrite(RELAY_C_R1, LOW);
+  }
+   else if (RFID1_A_R_status == LOW)
+  {
+        digitalWrite(RELAY_C_R1, HIGH); 
+  } 
+
+  
+   //-------------------------------RED ON RFID 3 BOARD D
+  if (RFID3_D_G_status == HIGH)
+  {
+    Serial.print("RFID3_D_G_status: ");
+    Serial.println(RFID3_D_G_status);
+    digitalWrite(RELAY_D_R2, LOW);
+  }
+   else if (RFID3_D_G_status == LOW)
+  {
+    digitalWrite(RELAY_D_R2, HIGH); 
+  } 
+  
+   //-------------------------------GREEN ON RFID 1 BOARD D
+  if (RFID3_D_R_status == HIGH)
+  {
+    Serial.print("RFID3_D_R_status: ");
+    Serial.println(RFID3_D_R_status);
+    digitalWrite(RELAY_D_G2, LOW);
+  }
+   else if (RFID3_D_G_status == LOW)
+  {
+    digitalWrite(RELAY_D_G2, HIGH); 
+  } 
+  //-------------------------------RED ON RFID 1 BOARD D
+ if (RFID1_D_G_status == HIGH)
+ {
+  Serial.print("RFID1_D_G_status: ");
+    Serial.println(RFID1_D_G_status);
+    digitalWrite(RELAY_D_G1, LOW);
+ }
+  else if (RFID1_D_G_status == LOW)
+  {
+    digitalWrite(RELAY_D_G1, HIGH); 
+  } 
+  //-------------------------------GREEN ON RFID 1 BOARD D
+  if (RFID1_D_R_status == HIGH)
+ {
+  Serial.print("RFID1_D_R_status: ");
+    Serial.println(RFID1_D_R_status);
+    digitalWrite(RELAY_D_R1, LOW);
+ }
+  else if (RFID1_D_R_status == LOW)
+  {
+    digitalWrite(RELAY_D_R1, HIGH); 
+  }   
 
 }
