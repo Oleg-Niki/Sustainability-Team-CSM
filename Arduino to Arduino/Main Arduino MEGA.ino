21b98adb3d97aa8ea70e72db893b07aae027a03f
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-09-03 11:35:51 -0700

    Updates from 9/3

--- Arduino to Arduino/Main Arduino MEGA.ino
+++ Arduino to Arduino/Main Arduino MEGA.ino
@@ -41,7 +41,7 @@ void setup()
   //start serial connection
   Serial.begin(9600);
   //configure pins as an input and enable the internal pull-up resistor
-  
+
 // INPUT PINS FOR BOARD D
   pinMode(RFID1_D_G, INPUT_PULLUP);
   pinMode(RFID1_D_R, INPUT_PULLUP);
@@ -74,16 +74,37 @@ void setup()
   pinMode(RELAY_C_G2, OUTPUT);
   pinMode(RELAY_C_R2, OUTPUT);
 
- 
-  // digitalWrite(2, LOW);
-  // digitalWrite(3, LOW);
-  // digitalWrite(4, LOW);
-  // digitalWrite(5, LOW);
-  // digitalWrite(6, HIGH);
-  // digitalWrite(7, HIGH);
-  // digitalWrite(8, HIGH);
-  // digitalWrite(9, HIGH);
-
+ //RELAYS AND LEDs TEST
+  //TESTING BOARD D
+  digitalWrite(RELAY_D_G1, LOW);
+  digitalWrite(RELAY_D_R1, LOW);
+  digitalWrite(RELAY_D_G2, LOW);
+  digitalWrite(RELAY_D_R2, LOW);
+  delay(1000);
+  digitalWrite(RELAY_D_G1, HIGH);
+  digitalWrite(RELAY_D_R1, HIGH);
+  digitalWrite(RELAY_D_G2, HIGH);
+  digitalWrite(RELAY_D_R2, HIGH);
+  delay(2000);
+  //TESTING BOARD A and C
+  // digitalWrite(RELAY_A_R1, HIGH);
+  // digitalWrite(RELAY_A_G1, HIGH);
+  // digitalWrite(RELAY_A_R2, HIGH);
+  // digitalWrite(RELAY_A_G2, HIGH);
+  // digitalWrite(RELAY_C_G1, HIGH);
+  // digitalWrite(RELAY_C_R1, HIGH);
+  // digitalWrite(RELAY_C_G2, HIGH);
+  // digitalWrite(RELAY_C_R2, HIGH);
+  // delay(1000);
+  // digitalWrite(RELAY_A_R1, LOW);
+  // digitalWrite(RELAY_A_G1, LOW);
+  // digitalWrite(RELAY_A_R2, LOW);
+  // digitalWrite(RELAY_A_G2, LOW);
+  // digitalWrite(RELAY_C_G1, LOW);
+  // digitalWrite(RELAY_C_R1, LOW);
+  // digitalWrite(RELAY_C_G2, LOW);
+  // digitalWrite(RELAY_C_R2, LOW);
+  // delay(1000);
 }
 
 void loop() {
@@ -93,45 +114,29 @@ void loop() {
   // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
   // button's pressed, and off when it's not:
 
-  //GREEN
+   //GREEN
   if (RFID1_D_G == 1) 
   {
-    Serial.print("RFID_A_1 pin 53: ");
-    Serial.println(RFID_A_1);
+    Serial.print("RFID1_D_G pin 53: ");
+    Serial.println(RFID1_D_G);
     digitalWrite(53, HIGH);
   }
-  else if (RFID_A_1 == 0) 
+  else if (RFID1_D_G == 0) 
   {
-    Serial.print("RFID_A_1 pin 53: ");
-    Serial.println(RFID_A_1);
+    Serial.print("RFID1_D_G pin 53: ");
+    Serial.println(RFID1_D_G);
     digitalWrite(53, LOW); 
   } 
     
     
   
   //RED
-  if (RFID_A_2 == 1) {
+  if (RFID1_D_R == 1) {
     digitalWrite(51, HIGH);
     } 
-    else if (RFID_A_2 == 0) { 
+    else if (RFID1_D_R == 0) { 
     digitalWrite(51, LOW); 
   }  
-  //GREEN
-  if (RFID_A_3 == 1) {
-    digitalWrite(49, HIGH);           
-    } 
-    else if (RFID_A_3 == 0){
-    digitalWrite(49, LOW);           
-    
-  }
-  //RED
-  if (RFID_A_4 == 1) {
-    digitalWrite(47, HIGH);
-    } 
-    else if (RFID_A_4 == 0){
-    digitalWrite(47, LOW); 
-    
-  }
 
 
 }
